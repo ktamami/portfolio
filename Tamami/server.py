2c29a8afda0622d02bc3b8942225c77cac26293a
Tamami <85936708+ktamami@users.noreply.github.com>
2021-11-11 16:59:24 +0900

    Add files via upload

--- Tamami/server.py
+++ Tamami/server.py
@@ -4,7 +4,10 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
+tamami = os.environ.get("TAMAMI")
 my_email = os.environ.get("MAIL_FROM")
 password = os.environ.get("PASS")
 address_list = [
@@ -21,7 +24,7 @@ def contact():
         name = request.form.get("name")
         email = request.form.get("email")
         message = request.form.get("message")
-        my_msg = MIMEText(f"おめでとう！たまみにメッセージが来たよ！\n"
+        my_msg = MIMEText(f"おめでとう！メッセージが来たよ！\n"
                           f"お名前：　　　　{name}さん\n"
                           f"メールアドレス：{email}\n"
                           f"メッセージ：　　{message}",
@@ -31,7 +34,7 @@ def contact():
             connection.starttls()
             connection.login(user=my_email, password=password)
             connection.sendmail(from_addr=my_email, to_addrs=address_list, msg=my_msg.as_string())
-        return render_template("index.html", year=current_year)
-    return render_template("index.html", year=current_year)
+        return render_template("index.html", year=current_year, tamami=tamami)
+    return render_template("index.html", year=current_year, tamami=tamami)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
