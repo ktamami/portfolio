45ae39a2b3a8b08d27347fddaa35d8dc074abb4a
Tamami <85936708+ktamami@users.noreply.github.com>
2022-03-20 14:02:13 +0900

    Add files via upload

---
+++ Tamami/notification_manager.py
@@ -0,0 +1,25 @@
+from twilio.rest import Client
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# This is for messaging me when ideco assets rises.
+
+TWILIO_SID = os.environ.get("TWILIO_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+TWILIO_FROM = os.environ.get("TWILIO_FROM")
+TWILIO_TO = os.environ.get("TWILIO_TO")
+
+
+class NotificationManager:
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_whatsapp(self, text):
+        message = self.client.messages.create(
+            from_=TWILIO_FROM,
+            body=text,
+            to=TWILIO_TO
+        )
+        print(message.sid)
--- Tamami/server.py
+++ Tamami/server.py
@@ -1,18 +1,11 @@
 from flask import Flask, render_template, request
 import datetime
-import smtplib
-from email.mime.text import MIMEText
-from email.header import Header
-import os
-from dotenv import load_dotenv
-load_dotenv()
+import notification_manager
 
-mail_from = os.environ.get("MAIL_FROM")
-password = os.environ.get("PASS")
-mail_to = os.environ.get("MAIL_TO")
-charset = "iso-2022-jp"
+notification_manager = notification_manager.NotificationManager()
 
 app = Flask(__name__)
+
 
 @app.route("/", methods=["GET", "POST"])
 def contact():
@@ -21,17 +14,14 @@ def contact():
         name = request.form.get("name")
         email = request.form.get("email")
         message = request.form.get("message")
-        my_msg = MIMEText(f"おめでとう！メッセージが来たよ！\n"
-                          f"お名前：　　　　{name}さん\n"
-                          f"メールアドレス：{email}\n"
-                          f"メッセージ：　　{message}",
-                          "plain", charset)
-        my_msg['Subject'] = Header(f"【新着メッセージ】{name}さん", charset)
-        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-            connection.starttls()
-            connection.login(user=mail_from, password=password)
-            connection.sendmail(from_addr=mail_from, to_addrs=mail_to, msg=my_msg.as_string())
+        text = f"📩 You got a message via 🍕\n\n" \
+               f"Name: {name} san\n" \
+               f"Email: {email}\n" \
+               f"Message: {message}"
+        notification_manager.send_whatsapp(text)
         return render_template("index.html", year=current_year)
     return render_template("index.html", year=current_year)
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
